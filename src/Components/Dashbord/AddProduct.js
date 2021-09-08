b249a641bbbce2fc15b6f6e330965c1d644f8bed
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-09-09 02:28:15 +0600

    Product varient is added

--- src/Components/Dashbord/AddProduct.js
+++ src/Components/Dashbord/AddProduct.js
@@ -1,4 +1,4 @@
-import { ArrowNarrowLeftIcon,UploadIcon } from "@heroicons/react/solid";
+import { ArrowNarrowLeftIcon, UploadIcon } from "@heroicons/react/solid";
 import JoditEditor from "jodit-react";
 import React, { useRef, useState } from "react";
 function AddProduct() {
@@ -59,28 +59,52 @@ const config = {
           />
         </div>
         <div className="mt-3">
-          <label
-            class="block font-bold md:text-right mb-1 md:mb-0 pr-4 flex"
-            for="inline-full-name mb-2"
-          >
-            Upload media
-          </label>
-          <div className="flex  items-center border-2   md:shadow-sm">
-            <input
+          <div className=" items-center   md:shadow-sm">
+            {/* <input
               className="flex-grow bg-transparent outline-none pl-5 text-base"
               id="inline-full-name"
-              type="text"
+              type="file"
               // value="Enter title here "
-              placeholder="Enter title here"
+              placeholder="Enter file here"
+            /> */}
+            <label
+              class="block font-bold md:text-right mb-1 md:mb-0 pr-4 flex"
+              for="file"
+            >
+              Upload media
+            </label>
+
+            <input
+              type="file"
+              className="flex-grow  border-2  bg-transparent outline-none pl-5"
+              id="file"
+              style={{ display: "none" }}
+              name="image"
+              accept="image/gif,image/jpeg,image/jpg,image/png"
+              multiple=""
+              data-original-title="upload photos"
             />
 
             <UploadIcon className=" h-10 w-10 text-black-500 inline-block justify-items-center pr-2 cursor-pointer" />
           </div>
-
+          <div className="mt-3">
+            <label
+              class="block font-bold md:text-right mb-1 md:mb-0 pr-4 flex"
+              for="file"
+            >
+              Product variant
+            </label>
+            <p className="text-gray-600 text-sm">
+              This product has multiple options,like different sizes or colors{" "}
+            </p>
+            <button class="text-gray-50 text-xs hover:bg-blue-700 text-white  py-2 px-4 bg-gray-800  rounded-md mt-3">
+              Add Variant Option
+            </button>
+          </div>
           <div className="mt-3">
             <label
               class="block font-bold md:text-right mb-1 pr-4 flex"
-              for="inline-full-name mb-2"
+              for="file"
             >
               SEO Meta Detail
             </label>
